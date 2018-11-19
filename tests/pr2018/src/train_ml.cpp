61f5d42fb6c9bec86c8012e7260d380bde79ca0b
dblumenthal <davidbenjamin.blumenthal@unibz.it>
2018-11-19 13:08:07 +0100

    updated train_ml

--- tests/pr2018/src/train_ml.cpp
+++ tests/pr2018/src/train_ml.cpp
@@ -41,26 +41,20 @@ void train_on_dataset(const std::string & dataset) {
 	// Train RingML and BipartiteML with DNN.
 	env.set_method(ged::Options::GEDMethod::RING_ML, util::init_options(dataset, "ring_ml_dnn", "ring_ml", true, false, 16) + util::ground_truth_option(dataset) + " --ml-method DNN --save-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
 	env.init_method();
-	if (dataset != "AIDS") {
-		env.set_method(ged::Options::GEDMethod::BIPARTITE_ML, util::init_options(dataset, "bipartite_ml_BIPARTITE_dnn", "bipartite_ml_BIPARTITE", true, false, 16) + " --ml-method DNN --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
-		env.init_method();
-	}
+	env.set_method(ged::Options::GEDMethod::BIPARTITE_ML, util::init_options(dataset, "bipartite_ml_BIPARTITE_dnn", "bipartite_ml_BIPARTITE", true, false, 16) + " --ml-method DNN --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
+	env.init_method();
 
 	// Train RingML and BipartiteML with OneClassSVM.
 	env.set_method(ged::Options::GEDMethod::RING_ML, util::init_options(dataset, "ring_ml_one_class_svm", "ring_ml_one_class", true, false, 16) + util::ground_truth_option(dataset) + " --ml-method ONE_CLASS_SVM --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
 	env.init_method();
-	if (dataset != "AIDS") {
-		env.set_method(ged::Options::GEDMethod::BIPARTITE_ML, util::init_options(dataset, "bipartite_ml_BIPARTITE_one_class_svm", "bipartite_ml_BIPARTITE_one_class", true, false, 16) + " --ml-method ONE_CLASS_SVM --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
-		env.init_method();
-	}
+	env.set_method(ged::Options::GEDMethod::BIPARTITE_ML, util::init_options(dataset, "bipartite_ml_BIPARTITE_one_class_svm", "bipartite_ml_BIPARTITE_one_class", true, false, 16) + " --ml-method ONE_CLASS_SVM --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
+	env.init_method();
 
-	// Train RingML and BipartiteML with OneClassSVM.
+	// Train RingML and BipartiteML with SVM.
 	env.set_method(ged::Options::GEDMethod::RING_ML, util::init_options(dataset, "ring_ml_svm", "ring_ml", false, true, 16) + util::ground_truth_option(dataset) + " --ml-method SVM --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
 	env.init_method();
-	if (dataset != "AIDS") {
-		env.set_method(ged::Options::GEDMethod::BIPARTITE_ML, util::init_options(dataset, "bipartite_ml_BIPARTITE_svm", "bipartite_ml_BIPARTITE", false, true, 16) + " --ml-method SVM --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
-		env.init_method();
-	}
+	env.set_method(ged::Options::GEDMethod::BIPARTITE_ML, util::init_options(dataset, "bipartite_ml_BIPARTITE_svm", "bipartite_ml_BIPARTITE", false, true, 16) + " --ml-method SVM --load-ground-truth " + util::config_prefix(dataset) + "ground_truth.data");
+	env.init_method();
 }
 
 int main(int argc, char* argv[]) {

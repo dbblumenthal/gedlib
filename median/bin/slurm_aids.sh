830be19e65d7983d766bb0765780a57cc00c6368
David B. Blumenthal <david.b.blumenthal@gmail.com>
2019-10-09 11:01:32 +0200

    slurm wrappers

---
+++ median/bin/slurm_aids.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=median_aids
+#SBATCH --output=median_aids.txt
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mail-type=end
+#SBATCH --mail-user=david.blumenthal@wzw.tum.de
+
+export OMP_NUM_THREADS=6
+
+srun --ntasks 1 --cpus-per-task $OMP_NUM_THREADS ./median_tests AIDS

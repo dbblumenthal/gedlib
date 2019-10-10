5661146fade6f92280a42a82f5846afaeee1fa74
David B. Blumenthal <david.b.blumenthal@gmail.com>
2019-10-10 14:50:54 +0200

    .

--- median/bin/slurm_muta.sh
+++ median/bin/slurm_muta.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=6
-#SBATCH --mail-type=end
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=david.blumenthal@wzw.tum.de
 
 export OMP_NUM_THREADS=6

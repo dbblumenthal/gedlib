89af9a5591b6defc24a2c66065b14f4356796114
David B. Blumenthal <david.b.blumenthal@gmail.com>
2020-03-14 15:11:50 +0100

    added time limit option to soa

--- median/soa-median/graph-lib_clean-median/script_xp_IBD.sh
+++ median/soa-median/graph-lib_clean-median/script_xp_IBD.sh
@@ -12,5 +12,5 @@ export OMP_NUM_THREADS=6
 
 for CLASS in C NO YES
 do
-	srun --ntasks 1 --cpus-per-task $OMP_NUM_THREADS ./test_VLDBJ_final/chemical-edit-distances ../../../data/datasets/IBD/data/IBD_${CLASS}.ds -q ../../../src/edit_costs/otu_distances.csv -m lsape_multi_bunke -i lsape_multi_bunke -p 1  > results/res_IBD_${CLASS}.csv
+	srun --ntasks 1 --cpus-per-task $OMP_NUM_THREADS ./test_VLDBJ_final/chemical-edit-distances ../../../data/datasets/IBD/data/IBD_${CLASS}.ds -q ../../../src/edit_costs/otu_distances.csv -m lsape_multi_bunke -i lsape_multi_bunke -p 1 -l 60000 > results/res_IBD_${CLASS}.csv
 done
